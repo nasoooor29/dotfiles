0398037aafebdb661335829d395a14f7d4c6b491
Naser <nasoooor29@gmail.com>
2024-10-16 17:03:37 +0300

    stupid+7

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,1 @@
 systemctl enable iwd && systemctl start iwd
-# while read pkg; do yay --noconfirm --answerclean no --answerdiff no --needed $pkg; done < scripts/pkgs.txt
-#
-#
-#
-#
-#
-yay  --answerclean   None --answerdiff    None --answeredit    None --answerupgrade None --noconfirm   --askyesremovemake -S $(cat scripts/pkgs.txt)
