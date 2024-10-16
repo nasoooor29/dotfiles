242bc1c67280eeaf63836284d589803ab221a1d4
Naser <nasoooor29@gmail.com>
2024-10-16 16:21:09 +0300

    stupid+5

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,3 @@
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 systemctl enable iwd && systemctl start iwd
 while read pkg; do yay -S --needed $pkg; done < scripts/pkgs.txt
 
