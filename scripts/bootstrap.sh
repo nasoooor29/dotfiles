f24f17cce2a7730ab719b35d24850484aaf8ae05
Naser <nasoooor29@gmail.com>
2024-10-16 16:31:26 +0300

    stupid+6

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,3 +1,8 @@
 systemctl enable iwd && systemctl start iwd
-while read pkg; do yay -S --noconfirm --answerclean no --answerdiff no --needed $pkg; done < scripts/pkgs.txt
-
+# while read pkg; do yay --noconfirm --answerclean no --answerdiff no --needed $pkg; done < scripts/pkgs.txt
+#
+#
+#
+#
+#
+LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" -S $(cat scripts/pkgs.txt)
