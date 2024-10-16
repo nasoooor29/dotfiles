4a4967a1c09288a94ed3afd89c753aba5f27fe0b
Naser <nasoooor29@gmail.com>
2024-10-16 16:18:23 +0300

    stupid+4

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,4 @@
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 systemctl enable iwd && systemctl start iwd
-systemctl enable sshd && systemctl start sshd
-sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
-
 while read pkg; do yay -S --needed $pkg; done < scripts/pkgs.txt
 
--- scripts/services.sh
+++ scripts/services.sh
@@ -1,3 +1,4 @@
-
+systemctl enable sshd && systemctl start sshd
+sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
 
 systemctl enable ly && systemctl start ly
