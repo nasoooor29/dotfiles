9fb4e291dd69a2cc11071fcbfbcce7232c1c4e2b
Nasoooor <Nasoooor29@gmail.com>
2024-10-20 23:49:32 +0300

    hardtime nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,3 +1,6 @@
+vim.keymap.set("n", "<leader>cht", "<cmd>Hardtime toggle<CR>", {
+	desc = "hardtime toggle",
+})
 vim.keymap.set("n", "<leader>cm", "<cmd>messages clear<CR>", {
 	desc = "clear messages",
 })
