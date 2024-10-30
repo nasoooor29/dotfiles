f3def723adfb3bbd37ce2de297f1934d1f822493
Nasoooor <Nasoooor29@gmail.com>
2024-10-30 20:42:00 +0300

    try something in term

--- .config/nvim/lua/plugins/toggle-term.lua
+++ .config/nvim/lua/plugins/toggle-term.lua
@@ -10,5 +10,8 @@ return {
 
 		vim.keymap.set("n", "<C-`>", "<cmd>ToggleTerm<CR>", { desc = "" })
 		vim.keymap.set("t", "<C-`>", "<cmd>ToggleTerm<CR>", { desc = "" })
+
+		vim.keymap.set("n", "<C-S-CR>", "<cmd>ToggleTerm size=20 direction=horizontal<CR>", { desc = "" })
+		vim.keymap.set("t", "<C-S-CR>", "<cmd>ToggleTerm size=20 direction=horizontal<CR>", { desc = "" })
 	end,
 }
