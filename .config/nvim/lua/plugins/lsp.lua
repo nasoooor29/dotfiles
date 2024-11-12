4ad0b8a17e19a6bca8627fd8b6b5ddc8a6272cf4
Nasoooor <Nasoooor29@gmail.com>
2024-11-12 22:08:44 +0300

    a

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,6 @@ return {
 		}, -- NOTE: Must be loaded before dependants
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim", -- Useful status updates for LSP.
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 		{
 			"j-hui/fidget.nvim",
 			opts = {},
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ return {
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>fee", "<cmd>Neotree focus<CR>", { desc = "Toggle neotree" })
+		vim.keymap.set("n", "<leader>fe", "<cmd>Neotree focus<CR>", { desc = "Toggle neotree" })
 		vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<CR>", { desc = "Toggle neotree" })
 	end,
 }
