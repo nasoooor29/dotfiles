3960de87eb8a57d6a625b74f32a329e2fb9e8033
Nasoooor <Nasoooor29@gmail.com>
2024-12-03 19:41:22 +0300

    add my repos to tmux

--- .zsh/scripts/sesh-mng.bash
+++ .zsh/scripts/sesh-mng.bash
@@ -1,16 +1,15 @@
+# --bind 'alt-3:change-prompt(  )+reload(sesh list -c)' \
 
 sesh connect "$(
-sesh list -t | fzf-tmux -p 60%,50% \
-    --no-sort --ansi --border-label ' sesh ' --prompt '  ' \
-    --header '  ^a tmux  ^x zoxide  ^d kill  ^f find  ^g configs ' \
-    --color border:#7DAEA3\
-    --layout=reverse\
-    --info=inline\
-    --header-first\
+sesh list -t | grep -v "scratch" | fzf-tmux -p 70%,60%  --border \
+    --header '  A-1 tmux  A-2 zoxide  A-3 My Repos  A-4 find  A-d kill ' \
+    --no-sort --ansi --border-label ' sesh ' --prompt '  ' --border=rounded \
     --bind 'tab:down,btab:up' \
-    --bind 'alt-a:change-prompt(  )+reload(sesh list -t)' \
-    --bind 'alt-g:change-prompt(  )+reload(sesh list -c)' \
-    --bind 'alt-x:change-prompt(  )+reload(sesh list -z)' \
-    --bind 'alt-f:change-prompt(  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-    --bind 'alt-d:execute(tmux kill-session -t {})+change-prompt(  )+reload(sesh list -t)'
+    --bind 'alt-1:change-prompt(  )+reload(sesh list -t | grep -v "scratch")' \
+    --bind 'alt-2:change-prompt(  )+reload(sesh list -z)' \
+    --bind 'alt-3:change-prompt(  )+reload(find "$HOME/repos/" -maxdepth 1 -type d)' \
+    --bind 'alt-4:change-prompt(  )+reload(find -H -d 2 -t d -E .Trash . ~)' \
+    --bind 'alt-d:execute(tmux kill-session -t {})+change-prompt(  )+reload(sesh list -t | grep -v "scratch")' \
+    --bind 'alt-k:up,alt-j:down'\
+    --bind alt-\':abort
 )"
