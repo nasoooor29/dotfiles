2bd4fc5b802c7b4be5e4b9289871dc826533bf11
Nasoooor <Nasoooor29@gmail.com>
2024-12-05 21:55:19 +0300

    hide command term from sesh

--- .config/tmux/scripts/sesh-mng.sh
+++ .config/tmux/scripts/sesh-mng.sh
@@ -1,6 +1,5 @@
 # --bind 'alt-3:change-prompt(  )+reload(sesh list -c)' \
 
-# just so it doesn't apear on my popup term
 CURRENT_SESSION=$(tmux display-message -p '#S')
 TERM_NAME="_COMMAND_TERM"
 
@@ -9,14 +8,15 @@ if [[ $CURRENT_SESSION == *"$TERM_NAME"* ]]; then
 fi
 
 sesh connect "$(
-    sesh list -t | grep -v "scratch" | fzf-tmux -p 60%,40% --border --margin=0 --header '  A-1 tmux  A-2 zoxide  A-3 My Repos  A-4 find  A-d kill ' \
+    sesh list -t | grep -v $TERM_NAME | fzf-tmux -p 60%,40% --border --margin=0 \
+        --header '  A-1 tmux  A-2 zoxide  A-3 My Repos  A-4 find  A-d kill ' \
         --no-sort --ansi --border-label ' sesh ' --prompt '  ' --border=rounded \
-        --bind 'tab:down,btab:up' \
-        --bind 'alt-1:change-prompt(  )+reload(sesh list -t | grep -v "scratch")' \
-        --bind 'alt-2:change-prompt(  )+reload(sesh list -z)' \
-        --bind 'alt-3:change-prompt(  )+reload(find "$HOME/repos/" -maxdepth 1 -type d)' \
-        --bind 'alt-4:change-prompt(  )+reload(find -H -d 2 -t d -E .Trash . ~)' \
-        --bind 'alt-d:execute(tmux kill-session -t {})+change-prompt(  )+reload(sesh list -t | grep -v "scratch")' \
-        --bind 'alt-k:up,alt-j:down' \
+        --bind "tab:down,btab:up" \
+        --bind "alt-1:change-prompt(  )+reload(sesh list -t | grep -v $TERM_NAME)" \
+        --bind "alt-2:change-prompt(  )+reload(sesh list -z)" \
+        --bind "alt-3:change-prompt(  )+reload(find $HOME/repos/ -maxdepth 1 -type d)" \
+        --bind "alt-4:change-prompt(  )+reload(find -H -d 2 -t d -E .Trash . ~)" \
+        --bind "alt-d:execute(tmux kill-session -t {})+change-prompt(  )+reload(sesh list -t | grep -v $TERM_NAME)" \
+        --bind "alt-k:up,alt-j:down" \
         --bind alt-\':abort
 )"
