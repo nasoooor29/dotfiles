f64919fd04ac7f0f9f96e3834745d72b31298a33
Nasoooor <Nasoooor29@gmail.com>
2024-12-05 21:21:12 +0300

    add functions

--- .config/tmux/scripts/toggle-term.sh
+++ .config/tmux/scripts/toggle-term.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ -z "$TMUX" ]]; then
     echo "This script must be run inside a Tmux session." >&2
     exit 1
@@ -7,18 +9,45 @@ CURRENT_SESSION=$(tmux display-message -p '#S')
 CURRENT_SESSION_PATH=$(tmux display-message -p -t :1 '#{pane_current_path}')
 TERM_NAME="_COMMAND_TERM"
 
-if [[ $CURRENT_SESSION == *"$TERM_NAME"* ]]; then
-    TERM_SESSION_NAME="${CURRENT_SESSION}"
-    # tmux set-option -t $TERM_SESSION_NAME status off
-    tmux detach-client -s $TERM_SESSION_NAME
+toggle() {
+    if [[ $CURRENT_SESSION == *"$TERM_NAME"* ]]; then
+        close
+    else
+        open
+    fi
     exit 0
-else
+}
+
+open() {
     TERM_SESSION_NAME="${CURRENT_SESSION}${TERM_NAME}"
+
+    if [[ $CURRENT_SESSION != *"$TERM_NAME"* ]]; then
+        tmux new-session -d -s "$TERM_SESSION_NAME" -c "$CURRENT_SESSION_PATH"
+    fi
+
+    tmux display-popup -h 75% -w 90% -E "tmux attach-session -t $TERM_SESSION_NAME"
+    exit 0
+}
+
+close() {
+    if [[ $CURRENT_SESSION != *"$TERM_NAME"* ]]; then
+        return
+    fi
+    TERM_SESSION_NAME="${CURRENT_SESSION}"
+    if tmux has-session -t "$TERM_SESSION_NAME" 2>/dev/null; then
+        tmux detach-client -s $TERM_SESSION_NAME
+    fi
+    exit 0
+}
+
+if [[ $1 == "open" ]]; then
+    open
+    exit 0
 fi
 
-if ! tmux has-session -t "$TERM_SESSION_NAME" 2>/dev/null; then
-    tmux new-session -d -s "$TERM_SESSION_NAME" -c "$CURRENT_SESSION_PATH"
+if [[ $1 == "close" ]]; then
+    close
+    exit 0
 fi
 
-# tmux set-option -t $TERM_SESSION_NAME status off
-tmux display-popup -h 75% -w 90% -E "tmux attach-session -t $TERM_SESSION_NAME"
+toggle
