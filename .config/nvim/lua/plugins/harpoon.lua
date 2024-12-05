e9baac303c1b41d7d7b0eb5571744cdd56cfbf13
Nasoooor <Nasoooor29@gmail.com>
2024-12-05 17:54:44 +0300

    fix the escape key

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,38 +1,44 @@
 return {
 	"ThePrimeagen/harpoon",
+	-- enabled = false,
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
-		harpoon:setup({})
-		vim.keymap.set("n", "<A-m>", function()
+		harpoon:setup()
+		vim.keymap.set("n", "<leader>m", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		vim.keymap.set("n", "<A-S-1>", function()
+		vim.keymap.set("n", "<leader>1", function()
 			harpoon:list():select(1)
 		end)
-		vim.keymap.set("n", "<A-S-2>", function()
+		vim.keymap.set("n", "<leader>2", function()
 			harpoon:list():select(2)
 		end)
-		vim.keymap.set("n", "<A-S-3>", function()
+		vim.keymap.set("n", "<leader>3", function()
 			harpoon:list():select(3)
 		end)
-		vim.keymap.set("n", "<A-S-4>", function()
+		vim.keymap.set("n", "<leader>4", function()
 			harpoon:list():select(4)
 		end)
-		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<leader>hn", function()
 			harpoon:list():prev()
 		end)
 		vim.keymap.set("n", "<leader>hp", function()
 			harpoon:list():next()
 		end)
-		vim.keymap.set("n", "<A-->", function()
+		-- bad idea
+		-- vim.keymap.set("n", "<C-]>", function()
+		-- 	harpoon:list():prev()
+		-- end)
+		-- vim.keymap.set("n", "<C-[>", function()
+		-- 	harpoon:list():next()
+		-- end)
+		vim.keymap.set("n", "<leader>-", function()
 			harpoon:list():remove()
 		end)
-
-		vim.keymap.set("n", "<A-0>", function()
+		vim.keymap.set("n", "<leader>=", function()
 			harpoon:list():add()
 		end)
 	end,
