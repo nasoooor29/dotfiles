76c57f6133fde20b174bcc4321948239188c5152
Nasoooor <Nasoooor29@gmail.com>
2024-12-09 13:58:08 +0300

    delete unused & BACKUP line and starter

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -2,23 +2,11 @@ return { -- Collection of various small independent plugins/modules
 	"echasnovski/mini.nvim",
 	version = false,
 	config = function()
-		-- Better Around/Inside textobjects
-		--
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]paren
-		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-		--  - ci'  - [C]hange [I]nside [']quote
 		require("mini.ai").setup({ n_lines = 500 })
-
-		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
-		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-		-- - sd'   - [S]urround [D]elete [']quotes
-		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
 		require("mini.move").setup()
 		require("mini.tabline").setup()
-		-- require("mini.starter").setup()
+		require("mini.starter").setup()
 		require("mini.pairs").setup()
 		require("mini.cursorword").setup()
 		require("mini.indentscope").setup({
@@ -26,9 +14,49 @@ return { -- Collection of various small independent plugins/modules
 			-- draw = {
 			-- 	animation = indent.gen_animation.none(),
 			-- },
-			--
 		})
+		-- TODO: Ask ebrahim about the the colors of #fffff
+		-- require("mini.hipatterns").setup({
+		-- 	highlighters = {
+		-- 		hex_color = hipatterns.gen_highlighter.hex_color(),
+		-- 	},
+		-- })
 
-		-- require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
+		require("mini.statusline").setup({
+			use_icons = vim.g.have_nerd_font,
+			content = {
+				active = function()
+					local check_macro_recording = function()
+						if vim.fn.reg_recording() ~= "" then
+							return "Recording @" .. vim.fn.reg_recording()
+						else
+							return ""
+						end
+					end
+
+					local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+					local git = MiniStatusline.section_git({ trunc_width = 40 })
+					local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+					local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+					-- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+					local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+					local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+					local location = MiniStatusline.section_location({ trunc_width = 200 })
+					local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+					local macro = check_macro_recording()
+
+					return MiniStatusline.combine_groups({
+						{ hl = mode_hl, strings = { mode } },
+						{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+						"%<", -- Mark general truncate point
+						{ hl = "MiniStatuslineFilename", strings = { filename } },
+						"%=", -- End left alignment
+						{ hl = "MiniStatuslineFilename", strings = { macro } },
+						{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+						{ hl = mode_hl, strings = { search, location } },
+					})
+				end,
+			},
+		})
 	end,
 }
