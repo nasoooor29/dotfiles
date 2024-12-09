6a9a93fe43ca4e2cea5bf2f73294b44361afc1a9
Nasoooor <Nasoooor29@gmail.com>
2024-12-09 20:44:09 +0300

    better multi cursors?

--- .config/nvim/lua/plugins/multi.lua
+++ .config/nvim/lua/plugins/multi.lua
@@ -1,3 +1,17 @@
+-- return {
+-- 	"mg979/vim-visual-multi",
+-- }
+
 return {
-	"mg979/vim-visual-multi",
+	"brenton-leighton/multiple-cursors.nvim",
+	version = "*", -- Use the latest tagged version
+	opts = {}, -- This causes the plugin setup function to be called
+	keys = {
+		{ "<C-A-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" } },
+		{ "<C-A-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" } },
+		{ "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" } },
+		{ "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" } },
+		{ "<leader>l", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" } },
+		{ "<C-d>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "n", "x" } },
+	},
 }
