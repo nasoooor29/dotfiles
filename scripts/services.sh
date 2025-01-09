a3a698819505835e0b4d431637b689e9ed3b14ac
Nasoooor <Nasoooor29@gmail.com>
2025-01-09 21:24:42 +0300

    progress

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,5 +1,4 @@
 systemctl enable sshd && systemctl start sshd
 sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
-
 systemctl enable ly && systemctl start ly
 systemctl enable docker && systemctl start docker
