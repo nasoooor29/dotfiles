359d8545c135a86f1848c2a9874eb297738a4745
Nasoooor <Nasoooor29@gmail.com>
2025-02-17 17:36:55 +0300

    nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,24 +6,33 @@ return {
 		{
 			"akinsho/horizon.nvim",
 			"folke/tokyonight.nvim",
-			"rose-pine/neovim",
+			"xiyaowong/transparent.nvim"
 		},
 	},
 
 	config = function()
-		-- vim.cmd.colorscheme("horizon")
 		vim.cmd.colorscheme("catppuccin-mocha")
-		-- vim.cmd.colorscheme("tokyonight-night")
-		-- vim.cmd.colorscheme("rose-pine")
+		require('transparent').clear_prefix('NeoTree')
+		require('transparent').clear_prefix('MiniStatusline')
+		vim.cmd("TransparentEnable")
+
 
 		local accent = "#cba6f7"
 		vim.diagnostic.config({
 			float = {
 				scope = "cursor",
 				border = "rounded",
-				max_width = 80, -- Set the maximum width for the floating window
+				max_width = 80,
 			},
 		})
+
+		vim.api.nvim_set_hl(0, "MiniStatuslineModeNormal", { fg = "white" })
+		vim.api.nvim_set_hl(0, "MiniStatuslineModeVisual", { fg = "white" })
+		vim.api.nvim_set_hl(0, "MiniStatuslineModeCommand", { fg = "white" })
+		vim.api.nvim_set_hl(0, "MiniStatuslineModeInsert", { fg = "white" })
+		vim.api.nvim_set_hl(0, "MiniStatuslineModeReplace", { fg = "white" })
+		vim.api.nvim_set_hl(0, "MiniStatuslineModeOther", { fg = "white" })
+		vim.api.nvim_set_hl(0, "NotifyBackground", { bg = accent })
 		vim.api.nvim_set_hl(0, "MiniTablineCurrent", { fg = accent })
 		vim.api.nvim_set_hl(0, "CursorLineNr", { fg = accent })
 		vim.api.nvim_set_hl(0, "FloatBorder", { fg = accent })
